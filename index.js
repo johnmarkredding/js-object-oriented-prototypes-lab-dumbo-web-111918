ebffb67ab9b9893ea616fab469d00a8871a75513
John Mark Redding <johnmarkredding@gmail.com>
2019-01-13 20:58:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,4 @@ function BoardMember(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-
-
-
-this.sayHi = function(){
-  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-}
 }
