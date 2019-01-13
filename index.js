7256afc0035151249d1b8ed4684d3f88c58eb40a
John Mark Redding <johnmarkredding@gmail.com>
2019-01-13 20:58:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,18 +7,16 @@ BoardMember.prototype.approve = function(){
 BoardMember.prototype.doCharity = function(){
   return "I like to help people."
 };
+BoardMember.prototype.releasePressStatement = function(){
+  return "You will see great things from Scuber."
+};
 
 function BoardMember(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
 
-  
-  this.
 
-  this.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-  }
 
   this.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
